baea073ede77911b7c82e0c95b8c8716a427c293
student <123>
2015-03-07 14:05:51 +0200

    Init commit

---
+++ app/src/main/java/com/webacadwmy/march/DetailActivity.java
@@ -0,0 +1,58 @@
+package com.webacadwmy.march;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class DetailActivity extends Activity {
+
+    public static final String EXTRA_KEY_MSG = "msg";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+
+        if(intent.hasExtra(EXTRA_KEY_MSG)) {
+            String  msg = intent.getStringExtra(EXTRA_KEY_MSG);
+            TextView textView = (TextView) findViewById(R.id.text);
+            textView.setText(msg);
+
+        }else {
+            Toast.makeText(this,"No data!", Toast.LENGTH_LONG ).show();
+        }
+
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}

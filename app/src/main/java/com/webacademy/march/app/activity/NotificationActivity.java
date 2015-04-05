5eaa96394040a502da10ae631d34dc23f2eb74f5
student <123>
2015-04-05 11:14:52 +0300

    Added animator

--- app/src/main/java/com/webacademy/march/app/activity/NotificationActivity.java
+++ app/src/main/java/com/webacademy/march/app/activity/NotificationActivity.java
@@ -1,5 +1,9 @@
 package com.webacademy.march.app.activity;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -8,6 +12,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnticipateInterpolator;
+import android.view.animation.AnticipateOvershootInterpolator;
+import android.view.animation.DecelerateInterpolator;
 
 import com.webacademy.march.R;
 
@@ -26,22 +36,23 @@ public class NotificationActivity extends Activity {
             @Override
             public void onClick(View v) {
 
-                NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(getBaseContext());
-                notifyBuilder.setSmallIcon(R.mipmap.ic_launcher);
-                notifyBuilder.setContentText("Hello Android");
-                notifyBuilder.setSubText("Sub");
+                ObjectAnimator animatorX = ObjectAnimator.ofFloat(v, View.SCALE_X, 1, 0.7f, 1);
+                ObjectAnimator animatorY = ObjectAnimator.ofFloat(v, View.SCALE_Y, 1, 0.7f, 1);
+                ObjectAnimator animatorTY = ObjectAnimator.ofFloat(v, View.TRANSLATION_Y, 0, -100);
 
-                Intent intent = new Intent(getBaseContext(), DBActivity.class);
-                PendingIntent pendingIntent = PendingIntent.getActivity(getBaseContext(), 1234, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                final AnimatorSet animatorSet = new AnimatorSet();
+                animatorSet.playTogether(animatorX, animatorY, animatorTY);
+                animatorSet.setInterpolator(new AnticipateOvershootInterpolator());
+                animatorSet.setDuration(3000);
 
-                notifyBuilder.setContentIntent(pendingIntent);
+                v.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        animatorSet.start();
+                    }
+                }, 800L);
 
-                notifyBuilder.addAction(new NotificationCompat.Action(R.mipmap.ic_launcher, "Cancel", pendingIntent));
 
-                Notification notification = notifyBuilder.build();
-
-                NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-                manager.notify(id++, notification);
             }
         });
 
